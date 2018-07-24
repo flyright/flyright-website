53fb465c809508adef90371f871cbc4601919f83
Ty <thopp@polity.tech>
2018-07-24 09:14:54 +0800

    Create isolated page for site links

---
+++ src/utils/links.js
@@ -0,0 +1,18 @@
+// Centralized source of links for site pages
+
+const links = {
+	about: '',
+	blog: '',
+	roadmap: '',
+	presskit: '',
+	android: '',
+	ios: '',
+	web: '',
+	api: '',
+	faq: '',
+	twitter: '',
+	terms: '',
+	privacy: '',
+}
+
+export default links
