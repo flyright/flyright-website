6cdda2c5a7719ee5e4523d3c1c3e8fe1c8319763
Ty <thopp@polity.tech>
2018-07-25 09:46:24 +0800

    Adjust styles for text again

--- src/components/textM.js
+++ src/components/textM.js
@@ -4,13 +4,16 @@ import media from '../utils/media'
 
 const TextM = styled.div`
 	font-family: Barlow, sans-serif;
-	font-size: 20px;
+	font-size: 19px;
 	line-height: 32px;
 	color: ${props => (props.color ? props.color : dark)};
 	letter-spacing: 0.2px;
 	padding: ${props => (props.padding ? props.padding : `0.5em 0`)};
 	text-align: ${props => (props.center ? `center` : `left`)};
-
+	${media.tab`
+		font-size: 20px;
+		line-height: 33px;
+	`};
 	${media.desk`
 		line-height: 34px;
 	`};
