440fd5ddfebc840a50d9470729e3f218c6bf652b
Benjamin Wallsten <ben@flyright.co>
2019-03-05 15:21:09 +0000

    Update author field to Flyright Org

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,7 +3,7 @@ require('dotenv').config()
 module.exports = {
   siteMetadata: {
     title: 'Flyright',
-    author: 'Ty Hopp',
+    author: 'Flyright, Inc.',
     siteUrl: 'https://flyright.co',
   },
   plugins: [
